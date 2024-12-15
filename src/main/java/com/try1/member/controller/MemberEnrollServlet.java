63d509e1285bb646e4fe57cfcfaffa3d0431fea1
hoonee-math <kwanghoon041@gmail.com>
2024-12-16 00:54:27 +0900

    gh/추가/회원가입 정보입력 페이지 추가 (상세 내용 확인)
    추가된 프론트 파일
    1. enrollMember.jsp //
    2. enrollMember.css
    
    추가된 백엔드 파일
    3. CheckEmailServlet // 보안 강화 버전. 회원가입 페이지에서 이메일 인증버튼 클릭을 통해 실행되는 요청
    4. EmailSenderService // 이메일 정보를 호출해서 메일을 보내주는 서비스 클래스
    5. EmailVerifyServlet // 사용자가 입력한 인증번호를 해시화하여 세션의 값과 비교하고, 인증 결과에 따른 처리를
    수행하는 서블릿
    
    [이메일 인증] 보안성 향상
    인증번호를 SHA-512로 해시화하여 세션에 저장
    유효시간 체크 (5분)
    실패 횟수 제한 (5회)
    세션 정보 관리 기능 통합
    인증 성공 시 emailVerified 상태 저장
    보안 강화 (원본 인증번호는 세션에 저장하지 않음)

---
+++ src/main/java/com/try1/member/controller/MemberEnrollServlet.java
@@ -0,0 +1,29 @@
+package com.try1.member.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/member/enrollmember.do")
+// 회원가입 버튼 클릭시 회원 가입 화면을 출력해주는 서블릿
+public class MemberEnrollServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public MemberEnrollServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/views/register/enrollMember.jsp").forward(request, response);
+		
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
