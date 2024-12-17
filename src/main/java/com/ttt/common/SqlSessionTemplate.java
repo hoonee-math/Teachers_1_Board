bb2b5a6705e040b8a04945201aba710b2aabbbef
이민영 <ffrf1234tina@gmail.com>
2024-12-17 23:33:44 +0900

    my/추가/resources폴더에 mapper 폴더 생성,driver.properties 생성,post-mapper.xml 생

--- src/main/java/com/ttt/common/SqlSessionTemplate.java
+++ src/main/java/com/ttt/common/SqlSessionTemplate.java
@@ -5,21 +5,17 @@ import java.io.InputStream;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 public class SqlSessionTemplate {
 	public static SqlSession getSession() {
-	      SqlSession session=null;
-	      String fileName="mybatis-config.xml";
-	      try(InputStream config=Resources.getResourceAsStream(fileName)){  
-	         SqlSessionFactoryBuilder sfb=new SqlSessionFactoryBuilder();
-	         SqlSessionFactory factory=sfb.build(config);
-	         session=factory.openSession(false);
-	         
-	      } catch(IOException e) {
-	         e.printStackTrace();
-	      }
-	      return session;
-	   }
+		SqlSession session = null;
+		String fileName="mybatis-config.xml";
+		try (InputStream is = Resources.getResourceAsStream(fileName)) {
+			session = new SqlSessionFactoryBuilder().build(is).openSession(false);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return session;
+	}
 }
