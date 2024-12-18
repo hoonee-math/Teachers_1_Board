e073d26f822cf631e2889da74f0e93efb6787a0b
이민영 <ffrf1234tina@gmail.com>
2024-12-18 16:43:04 +0900

    my/생성/comment dto 생성

---
+++ src/main/java/com/ttt/dao/Comment1.java
@@ -0,0 +1,13 @@
+package com.ttt.dao;
+
+public class Comment1 {
+	private int commentNo;
+	private int parentNo;
+	private String comment;
+	private Date createDate;
+	private Date updateDate;
+	private int level;
+	private Member1 member;
+	private Post1 post;
+
+}
