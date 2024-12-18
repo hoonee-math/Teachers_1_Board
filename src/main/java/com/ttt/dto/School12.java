1ffbff4a94f1b2ff26e9fc80d27c348829beefed
이민영 <ffrf1234tina@gmail.com>
2024-12-18 17:27:14 +0900

    my/수정/school dto 수정

--- src/main/java/com/ttt/dto/School12.java
+++ src/main/java/com/ttt/dto/School12.java
@@ -1,6 +1,12 @@
 package com.ttt.dto;
 
 public class School12 {
-	
+	private int schoolNo;
+	private String region;
+	private String district;
+	private String schoolName;
+	private int schoolType;
+	private int regionCode;
+	private int standardCode;
 
 }
