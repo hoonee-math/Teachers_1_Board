517d48c546da662527aaa070aef664220ff11abf
hoonee-math <kwanghoon041@gmail.com>
2024-12-21 20:23:45 +0900

    gh/오류 해결/config 오타로 인한 오류. 문제 해결 완료/ *** my 수정 제안 (상세 내용확인)
    post-mapper의 selectSchool 쿼리문에서 변수로 사용된 schoolType은 DB상에서는 숫자값임.
    여기서는 매개변수로 문자로 받아왔기 때문에 name에서 사용한것으로 보임.
    문제는 없지만. 주의 요함.

--- src/main/java/com/ttt/dto/School12.java
+++ src/main/java/com/ttt/dto/School12.java
@@ -1,11 +1,20 @@
 package com.ttt.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class School12 {
 	private int schoolNo;
 	private String region;
 	private String district;
 	private String schoolName;
-	private int schoolType;
+	private int schoolType; /* 1:초등학교 2:중학교 3:고등학교 4:기타학교 */ 
 	private int regionCode;
 	private int standardCode;
 
