192a0252d8d09c4491f1064229ec7703b1b5eb3b
hoonee-math <kwanghoon041@gmail.com>
2024-12-21 19:53:03 +0900

    gh/수정/school 연결 설정중. 글쓰기 school 관련 로직 미작동 해결을 위해 임시 저장

--- src/main/java/com/ttt/dao/SchoolDao.java
+++ src/main/java/com/ttt/dao/SchoolDao.java
@@ -1,5 +1,16 @@
 package com.ttt.dao;
 
-public class SchoolDao {
+import java.util.List;
+import java.util.Map;
 
+import org.apache.ibatis.session.SqlSession;
+
+import com.ttt.dto.School12;
+
+/* 서비스 클래스로부터 session 을 넘겨받아 쿼리문을 실행하는 클래스 */
+public class SchoolDao {
+	
+	public List<School12> selectNameAndCode(SqlSession session, Map<String,String> inputInfo){
+		return session.selectList("school12.selectNameAndCode",inputInfo);
+	}
 }
--- src/main/java/com/ttt/service/SchoolService.java
+++ src/main/java/com/ttt/service/SchoolService.java
@@ -1,5 +1,23 @@
 package com.ttt.service;
 
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.ttt.dao.SchoolDao;
+import com.ttt.dto.School12;
+
 public class SchoolService {
+
+	private SchoolDao dao=new SchoolDao();
+	
+	public List<School12> selectNameAndCode(Map<String,Integer> param) {
+		SqlSession session=getSession();
+		List<School12> result=dao.selectBoardLsit(session,param);
+		session.close();
+		return result;
+		
+	}
 
 }
