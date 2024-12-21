d2c9d5be15e21b58c16b01acddb0998bd98b5c34
이민영 <ffrf1234tina@gmail.com>
2024-12-22 00:15:43 +0900

    my/수정/게시글 백엔드 진행하려다가 객체를 빌더를 통해 dto에 저장하는 과정에서 막혔습니다,, 월요일에 학원에서 다시 진행하며 모르는 부분 바로 질문할게요!

--- src/main/java/com/ttt/controller/post/UploadEndServlet.java
+++ src/main/java/com/ttt/controller/post/UploadEndServlet.java
@@ -9,7 +9,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.oreilly.servlet.MultipartRequest;
-import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+import com.ttt.common.CustomFileRenamePolicy;
+import com.ttt.dto.Post1;
 
 /**
  * Servlet implementation class UploadEndServlet
@@ -25,20 +26,35 @@ public class UploadEndServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String path = getServletContext().getRealPath("/resources/upload/post");
 		
+		int memberNo = Integer.parseInt(request.getParameter("memberNo"));
+		int postNo = Integer.parseInt(request.getParameter("postNo"));
+		int imgOrder = Integer.parseInt(request.getParameter("imgOrder"));
+			//jsp 파일에서 hidden 처리해서 세 개 데이터 가져와야함 
+		
 		MultipartRequest mr = new MultipartRequest(
-				request, path, 1024*1024*100, "utf-8", new DefaultFileRenamePolicy()
+				request, path, 1024*1024*100, "utf-8", new CustomFileRenamePolicy(memberNo, postNo, imgOrder)
 				);
 		
 		String category = mr.getParameter("category");
-		String region = mr.getParameter("region");
-		String district = mr.getParameter("district");
-		String schoolName = mr.getParameter("schoolName");
+		//아래의 세 정보는 category에 따라 받는 정보라서 어떻게 전송해야할지 모르겠어요..
+//		String region = mr.getParameter("region");
+//		String district = mr.getParameter("district");
+//		String schoolName = mr.getParameter("schoolName");
 		String postTitle = mr.getParameter("postTitle");
 		String postWriter = mr.getParameter("postWriter");
 		String postContent = mr.getParameter("postContent");
 		
 		String oriname = mr.getOriginalFileName("upfile");
 		String rename = mr.getFilesystemName("upfile");
+		
+		//객체로 저장되어있는 정보들을 가져오는 
+		Post1 p = Post1.builder()
+				.category(category)
+				.postTitle(postTitle)
+//				.postWriter(postWriter)
+				.postContent(postContent)
+//				.rename(rename)
+				.build();
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
